5e19d71c2788a1b36d21b7cc8718b4af02b986c5
bryonverdone <bmverdone@gmail.com>
2022-12-12 12:57:45 -0500

    added layout

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,5 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Home</h1>
-    </>
-  );
+  return <></>;
 }
