d00e5f996795dfa77b97473e210bb1118f29239c
bryonverdone <bmverdone@gmail.com>
2022-12-12 12:56:58 -0500

    added nav and footer to layout component

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <h1 className='text-3xl font-bold underline text-center'>
+      Dan's Awards & Engraving
+    </h1>
+  );
+};
+
+export default Navbar;
